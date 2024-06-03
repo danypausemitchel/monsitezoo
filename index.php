22bce03539344a167aa431dbe0a1cdadd5c9559f
Dany PAUSE <chapolapaille974@gmail.com>
2024-06-03 12:38:40 +0400

    premier commit

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<?php
+require_once __DIR__. "/templates/header.php";
+?>
+
+<body>
+<div class="container col-xxl-8 px-4 py-5">
+        <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+            <div class="col-10 col-sm-8 col-lg-6">
+                <img src="assets/images/zoo.jpg" class="d-block mx-lg-auto img-fluid" alt="zoo" width="300">
+            </div>
+            <div class="col-lg-6">
+                <h1 class="display-5 fw-bold lh-1 mb-3">Gardez vos listes avec vous !</h1>
+                <p class="lead">Bienvenue sur CheckIt, votre nouvelle plateforme de création de listes de tâches en ligne. Avec CheckIt, vous pouvez facilement créer des listes de choses à faire pour tous les aspects de votre vie. Que vous planifiez votre prochain voyage, que vous organisiez votre travail ou que vous fassiez des courses, CheckIt vous aide à rester organisé et à suivre vos tâches en toute simplicité.</p>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
